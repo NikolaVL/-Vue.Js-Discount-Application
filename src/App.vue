0fb37e2b46ab37cf1cf71ce6735914b373da1fec
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2020-10-01 11:15:34 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container class="bv-example-row d-flex flex-column align-items-center">
+      <h1>{{mainMessage}}</h1>
+      <b-row class="w-50 mb-1">
+        <b-col class="p-0 d-flex align-items-center  ">
+          <b-form-input class="text-center"
+            v-model="price"
+            placeholder="Original Price"
+          ></b-form-input>
+        </b-col>
+      </b-row>
+      <b-row class="w-50 mb-1">
+        <b-col class="p-0 text-center d-flex align-items-center">
+          <b-form-input class="text-center"
+            v-model="discountPrice"
+            placeholder="What type of discount?"
+          ></b-form-input>
+        </b-col>
+      </b-row>
+      <b-row class="w-50 mb-1">
+        <b-button variant="primary" class="w-100" @click="displayYourePay">
+          CALCULATE
+        </b-button>
+      </b-row>
+      <b-row class="w-50 mb-1">
+        <b-button variant="danger" class="w-100" @click="displayPercentDiscount">
+          CALL : YOU'RE SAVING
+        </b-button>
+      </b-row>
+  <!--    <b-row class="w-50 text-left mb-1 bg-primary p-2 text-white">
+        <b-col class="p-0">
+          <div><span>DISCOUNT% : </span></div>
+        </b-col>
+        <b-col class="p-0">
+          <span>{{ result }} $ : </span>
+        </b-col>
+    </b-row>   !-->
+
+      <b-row class="w-50 mb-1 bg-secondary text-white">
+        <b-col class="p-2">
+          You're Pay : {{result}} $
+        </b-col>
+      </b-row>
+      <b-row class="w-50 mb-1 bg-secondary text-white">
+        <b-col class="p-2">
+          You're Saving : {{discount}}  $
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function () {
+    return {
+      price: null,
+      discountPrice: null,
+      result: null,
+      mainMessage:'DISCOUNT CALCULATOR',
+      discount:0,
+    };
+  },
+  methods: {
+    displayYourePay: function () {
+      return (this.result = this.price - (this.price * this.discountPrice) / 100);
+    },
+    displayPercentDiscount:function(){
+      return this.discount=this.result-this.price;
+    }
+  },
+};
 </script>
 
 <style>
